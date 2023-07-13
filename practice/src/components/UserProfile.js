8d18cb2ca16dce97c90d0fdabfd90b3da2b87b35
meet_hansalia <meethansalia99@gmail.com>
2023-07-13 17:39:30 +0530

    initial setup

---
+++ practice/src/components/UserProfile.js
@@ -0,0 +1,11 @@
+import './UserProfile.css';
+
+const UserProfile = () => {
+  return (
+    <main className={classes.profile}>
+      <h2>My User Profile</h2>
+    </main>
+  );
+};
+
+export default UserProfile;
